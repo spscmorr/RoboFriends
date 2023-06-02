68d61598a8da7d224e6d4b6e8ff7688817fe87ae
Christopher Morris <christophermorris@christohersmini.mynetworksettings.com>
2023-06-02 11:57:44 -0400

    Updates

---
+++ precache-manifest.8886f4182d93faa39dcaa1a713650584.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "af57aa9e197050b09e3e",
+    "url": "/RoboFriends/static/css/main.1ad7c10f.chunk.css"
+  },
+  {
+    "revision": "af57aa9e197050b09e3e",
+    "url": "/RoboFriends/static/js/main.af57aa9e.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/RoboFriends/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "8ba0b0e909881e9bea9f",
+    "url": "/RoboFriends/static/css/2.973d5be8.chunk.css"
+  },
+  {
+    "revision": "8ba0b0e909881e9bea9f",
+    "url": "/RoboFriends/static/js/2.8ba0b0e9.chunk.js"
+  },
+  {
+    "revision": "3323bec3596dbc4ac020",
+    "url": "/RoboFriends/static/js/runtime~main.3323bec3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "eb136b7564c39531558b943f842120f2",
+    "url": "/RoboFriends/index.html"
+  }
+];
